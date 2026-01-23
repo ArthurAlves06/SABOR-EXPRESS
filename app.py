6d25548b23201442e6f6ee28edca18d3814b2d4d
Arthur <161658611+ArthurAlves06@users.noreply.github.com>
2026-01-23 15:15:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
+    print('3. Alterar status do restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
@@ -34,10 +34,26 @@ def opcaoes_invalidas():
 
 def exibir_subtitulo(texto):
     os.system('cls')
+    linha = '*' * (len(texto)+4)
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
 def cadastrar_restaurante():
+    '''Essa função é responsável por cadastrar um novo 
+    restaurante no sistema.
+    
+    Inputs:
+    - Nome do restaurante
+    - Categoria do restaurante
+
+
+    Outputs:
+    - Adiona um novo restaurante na lista de restaurantes
+    
+    
+    ''' 
     exibir_subtitulo('Cadastro de restaurante')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
@@ -49,11 +65,14 @@ def cadastrar_restaurante():
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
+    exibir_subtitulo('Lista de restaurantes cadastrados')
+
+    print(f'{"Nome do Restaurante".ljust(22)} | {"Categoria".ljust(20)} | Status')
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
-        ativo = restaurante['ativo']
-        print(f'- {nome_restaurante} | {categoria} | {ativo}')
+        ativo = 'ativado ' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
 
     voltar_ao_menu_principal()
 
